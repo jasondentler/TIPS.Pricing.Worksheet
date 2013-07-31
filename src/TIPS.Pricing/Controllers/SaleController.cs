79440f045234e450ed7c576503552a69caad9521
Jason Dentler <jasondentler@gmail.com>
2013-07-31 17:40:07 -0500

    Initial commit

---
+++ src/TIPS.Pricing/Controllers/SaleController.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+
+namespace TIPS.Pricing.Controllers
+{
+    public class SaleController : ApiController 
+    {
+
+
+
+    }
+}
---
+++ src/TIPS.Pricing/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TIPS.Pricing.WebApiApplication" Language="C#" %>
