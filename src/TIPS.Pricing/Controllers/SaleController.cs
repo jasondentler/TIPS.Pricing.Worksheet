180c4a6c3c2e6dc39d8db2af91c395047d3914c6
Jason Dentler <jasondentler@gmail.com>
2013-07-31 20:20:32 -0500

    Bootstrap all the things

--- src/TIPS.Pricing/Controllers/SaleController.cs
+++ src/TIPS.Pricing/Controllers/SaleController.cs
@@ -1,15 +1,41 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
 using System.Web.Http;
+using TIPS.Pricing.Models;
 
 namespace TIPS.Pricing.Controllers
 {
     public class SaleController : ApiController 
     {
+        public IEnumerable<SaleSummary> Get(string q)
+        {
+            yield return new SaleSummary()
+            {
+                SaleId = 1,
+                JobNumber = "12340001",
+                CustomerName = "Ward and June Cleaver",
+                Address = "555 Cinco Road",
+                Community = "Cinco Ranch 55'"
+            };
 
+            yield return new SaleSummary()
+            {
+                SaleId = 2,
+                JobNumber = "12340001",
+                CustomerName = "Showcase",
+                Address = "555 Cinco Road",
+                Community = "Cinco Ranch 55'"
+            };
 
+            yield return new SaleSummary()
+            {
+                SaleId = 3,
+                JobNumber = "12340002",
+                CustomerName = "George and Judy Jetson",
+                Address = "556 Cinco Road",
+                Community = "Cinco Ranch 55'"
+            };
+
+        }
 
     }
 }
---
+++ src/TIPS.Pricing/Models/SaleSummary.cs
@@ -0,0 +1,11 @@
+﻿namespace TIPS.Pricing.Models
+{
+    public class SaleSummary
+    {
+        public int SaleId { get; set; }
+        public string JobNumber { get; set; }
+        public string CustomerName { get; set; }
+        public string Address { get; set; }
+        public string Community { get; set; }
+    }
+}
