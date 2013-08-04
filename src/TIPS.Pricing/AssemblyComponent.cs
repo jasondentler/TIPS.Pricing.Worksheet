7d3866881c588283e2b9c894c1e7aaf1b7d6e7ea
Jason Dentler <jasondentler@gmail.com>
2013-08-04 12:34:44 -0500

    Swing doesn't affect assembly component's product type

--- src/TIPS.Pricing/AssemblyComponent.cs
+++ src/TIPS.Pricing/AssemblyComponent.cs
@@ -2,10 +2,12 @@
 {
     public class AssemblyComponent
     {
+        /// <remarks>This may be the component item or its opposite, depending on the swing</remarks>
         public Item Item { get; set; }
         
         public decimal QuantityMultiplier { get; set; }
+
+        /// <remarks>Always the product type of the component item, regardless of swing</remarks>
         public string ProductType { get; set; }
     }
-
 }
